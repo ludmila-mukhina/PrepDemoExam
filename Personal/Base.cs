586ccd72cdc0928ce001f9dd284e44ce420fd25b
МухинаЛВ@ngknn.local <@ngknn.local>
2022-01-11 12:49:33 +0300

    Добавьте файлы проекта.

---
+++ Personal/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal
+{
+    class Base
+    {
+        public static PersonalEntities BaseConnect;
+    }
+}
